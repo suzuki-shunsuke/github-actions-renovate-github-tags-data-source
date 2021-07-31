281655b49cfa7fa3a7b1d1447ad91d9228f2229c
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-07-31 11:12:30 +0900

    fix: specify shell and replace versions.txt into $VERSION_FILE

--- update.sh
+++ update.sh
@@ -5,21 +5,21 @@ set -o pipefail
 
 git pull --tags
 git tag > tags.txt
-if ! grep -v -f tags.txt versions.txt; then
+if ! grep -v -f tags.txt "$VERSION_FILE"; then
   echo "===> No update" >&2
   exit 0
 fi
 
-if git diff --exit-code versions.txt; then
+if git diff --exit-code "$VERSION_FILE"; then
   ghcp empty-commit -r "$GITHUB_REPOSITORY" -m "update"
 else
-  ghcp commit -r "$GITHUB_REPOSITORY" -m "update versions.txt" versions.txt
+  ghcp commit -r "$GITHUB_REPOSITORY" -m "update $VERSION_FILE" "$VERSION_FILE"
 fi
-git checkout -- versions.txt
+git checkout -- "$VERSION_FILE"
 
 # push tags
 git pull
-grep -v -f tags.txt versions.txt | while read -r version; do
+grep -v -f tags.txt "$VERSION_FILE" | while read -r version; do
   git tag "$version"
   git push origin "$version"
 done
